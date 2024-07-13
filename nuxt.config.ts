cb1fabe299fb90aae1691ddbac7a967e93e5b046
Aaron Dewes <aaron@nirvati.org>
2024-07-13 13:28:43 +0200

    Fixup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,4 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   modules: ["@nuxtjs/tailwindcss"],
-  app: {
-    baseURL: process.env.BASE_URL || "http://localhost:3000",
-  }
 });
