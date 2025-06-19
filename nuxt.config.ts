e249c97cc6bb0f1bbc39847244a3072103cda776
Aaron Dewes <aaron@nirvati.org>
2025-06-19 20:46:28 +0200

    Add posthog

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,8 +2,12 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss"],
+  modules: ["@nuxtjs/tailwindcss", "nuxt-posthog"],
   app: {
     baseURL: process.env.BASE_URL || "",
-  }
+  },
+  posthog: {
+    publicKey: "phc_uChaVzjjHzXz7zVu5LOKZKPwIXEa6ZDsmDmDJg1hks2",
+    host: "https://eu.i.posthog.com",
+  },
 });
